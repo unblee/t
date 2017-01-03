2f2fa35ee8a713cba842830c645dccbad6725994
unblee <unblee@users.noreply.github.com>
2017-01-04 00:18:10 +0900

    Somehow completed :stuck_out_tongue_closed_eyes:

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"errors"
 	"flag"
 	"fmt"
@@ -13,6 +15,7 @@ import (
 	"os"
 	"path"
 	"strings"
+	"time"
 
 	"github.com/mattn/go-isatty"
 )
@@ -48,6 +51,8 @@ func main() {
 }
 
 func run(args []string) int {
+	logger := log.New(os.Stderr, "[ERROR] ", log.LstdFlags)
+
 	var text string
 	if isatty.IsTerminal(os.Stdin.Fd()) { // with Args
 		if flag.NArg() == 0 {
@@ -59,23 +64,35 @@ func run(args []string) int {
 		b, _ := ioutil.ReadAll(os.Stdin)
 		text = string(b)
 	}
-	model, source, target := detectLang(text)
-	reqBody := &requestBody{
-		ModelID: model,
-		Source:  source,
-		Target:  target,
-		Text:    []string{text},
-	}
-	return ExitCodeOK
-}
 
-func detectLang(text string) (model, source, target string) {
-	for _, c := range text {
-		if c > 127 {
-			return "ja-en", "ja", "en"
-		}
+	reqBody := newRequestBody(text)
+
+	reqJSON, err := json.Marshal(reqBody)
+	if err != nil {
+		logger.Println(err)
+		return ExitCodeError
 	}
-	return "en-ja", "en", "ja"
+
+	username := os.Getenv("T_WATSON_LANGUAGE_TRANSLATOR_API_USERNAME")
+	password := os.Getenv("T_WATSON_LANGUAGE_TRANSLATOR_API_PASSWORD")
+	client, err := newClient(username, password)
+	if err != nil {
+		logger.Println(err)
+		return ExitCodeError
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	result, err := client.translate(ctx, bytes.NewReader(reqJSON))
+	if err != nil {
+		logger.Println(err)
+		return ExitCodeError
+	}
+
+	fmt.Print(result)
+
+	return ExitCodeOK
 }
 
 type Client struct {
@@ -105,6 +122,36 @@ func newClient(username, password string) (*Client, error) {
 	}, nil
 }
 
+func (c *Client) translate(ctx context.Context, body io.Reader) (string, error) {
+	req, err := c.newRequest(ctx, "POST", "/translate", body)
+	if err != nil {
+		return "", err
+	}
+
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		errBody := new(errorResponseBody)
+		decodeBody(resp, errBody)
+
+		code := resp.StatusCode
+		switch code {
+		case 400, 404, 500:
+			return "", fmt.Errorf("\"code\":%d, \"error_message\":\"%s\"", code, errBody.ErrorMessage)
+		default:
+			return "", fmt.Errorf("\"code\":%d, \"error\":\"%s\", \"description\":\"%s\"", resp.StatusCode, errBody.Error, errBody.Description)
+		}
+	}
+
+	respBody := new(responseBody)
+	decodeBody(resp, respBody)
+	return respBody.Translations[0].Translation, nil
+}
+
 func (c *Client) newRequest(ctx context.Context, method, spath string, body io.Reader) (*http.Request, error) {
 	u := *c.url
 	u.Path = path.Join(c.url.Path, spath)
@@ -123,9 +170,48 @@ func (c *Client) newRequest(ctx context.Context, method, spath string, body io.R
 	return req, nil
 }
 
+func decodeBody(resp *http.Response, out interface{}) error {
+	decoder := json.NewDecoder(resp.Body)
+	return decoder.Decode(out)
+}
+
 type requestBody struct {
 	ModelID string   `json:"model_id"`
 	Source  string   `json:"source"`
 	Target  string   `json:"target"`
 	Text    []string `json:"text"`
+}
+
+func newRequestBody(text string) *requestBody {
+	model, source, target := detectLang(text)
+	return &requestBody{
+		ModelID: model,
+		Source:  source,
+		Target:  target,
+		Text:    []string{text},
+	}
+}
+
+func detectLang(text string) (model, source, target string) {
+	for n, c := range text {
+		if n > 20 {
+			break
+		}
+		if c > 127 {
+			return "ja-en", "ja", "en"
+		}
+	}
+	return "en-ja", "en", "ja"
+}
+
+type responseBody struct {
+	Translations []struct {
+		Translation string `json:"translation"`
+	} `json:"translations"`
+}
+
+type errorResponseBody struct {
+	ErrorMessage string `json:"error_message"`
+	Error        string `json:"error"`
+	Description  string `json:"description"`
 }
